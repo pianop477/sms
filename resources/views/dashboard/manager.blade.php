7141b24e80b97955705a97bafc418b69d4dfc279
piano <pianop477@gmail.com>
2025-01-07 00:56:55 +0300

    changes for day 6 january 2025 charts

--- resources/views/dashboard/manager.blade.php
+++ resources/views/dashboard/manager.blade.php
@@ -64,7 +64,7 @@
                                 {{count($students)}}
                             @endif
                         </h2>
-                        <ul>
+                        <ul style="font-size: 10px;">
                             <li><span class="text-white text-sm">M:
                                 <strong>
                                     @if ($maleStudents > 99)
@@ -145,5 +145,107 @@
     </div>
 </div>
 <hr class="dark horizontal py-0">
+<div class="col-lg-12">
+    <div class="row">
+        <div class="col-md-6 mt-5 mb-3">
+            <div class="card">
+                <div id="studentsChart" style="height: 400px;"></div>
+                <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
+                <script>
+                    const chartDom = document.getElementById('studentsChart');
+                    const myChart = echarts.init(chartDom);
+
+                    const option = {
+                        title: {
+                            text: 'Students by Classes'
+                        },
+                        tooltip: {},
+                        xAxis: {
+                            type: 'category',
+                            data: @json($studentsByClass->pluck('class_code'))
+                        },
+                        yAxis: {
+                            type: 'value',
+                            min: 0, // Minimum value on the y-axis
+                            interval: 5, // Set interval to 1
+                            axisLabel: {
+                                formatter: '{value}' // Format values as integers
+                            }
+                        },
+                        series: [{
+                            data: @json($studentsByClass->pluck('student_count')),
+                            type: 'bar',
+                            itemStyle: {
+                                color: '#5470C6'
+                            }
+                        }]
+                    };
+
+                    myChart.setOption(option);
+                </script>
+            </div>
+        </div>
+        <div class="col-md-6 mt-5 mb-3">
+            <div class="card">
+                <div id="qualificationChart" style="width: 100%; height: 400px;"></div>
+
+                <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+                <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+                <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+                <script>
+                    am5.ready(function() {
+                        // Create root element
+                        var root = am5.Root.new("qualificationChart");
+
+                        // Set themes
+                        root.setThemes([am5themes_Animated.new(root)]);
+
+                        // Create chart
+                        var chart = root.container.children.push(
+                            am5percent.PieChart.new(root, {
+                                layout: root.verticalLayout
+                            })
+                        );
+
+                        // Add chart title
+                        chart.children.unshift(
+                            am5.Label.new(root, {
+                                text: "Teachers by Qualifications",
+                                fontSize: 20,
+                                fontWeight: "bold",
+                                textAlign: "center",
+                                x: am5.percent(50),
+                                centerX: am5.percent(50)
+                            })
+                        );
+
+                        // Create series
+                        var series = chart.series.push(
+                            am5percent.PieSeries.new(root, {
+                                valueField: "value",
+                                categoryField: "category"
+                            })
+                        );
+
+                        // Add data
+                        series.data.setAll([
+                            { category: "Masters", value: {{ $qualificationData['masters'] }} },
+                            { category: "Degree", value: {{ $qualificationData['bachelor'] }} },
+                            { category: "Diploma", value: {{ $qualificationData['diploma'] }} },
+                            { category: "Certificate", value: {{ $qualificationData['certificate'] }} }
+                        ]);
+
+                        // Add legend
+                        chart.children.push(am5.Legend.new(root, {}));
+
+                        // Animate chart
+                        series.appear(1000, 100);
+                    });
+                </script>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
+
