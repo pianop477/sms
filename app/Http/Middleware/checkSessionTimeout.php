db8fd4ac7ed9270221c38cf208e9184b7b6d1a93
piano <pianop477@gmail.com>
2025-01-08 01:44:27 +0300

    set session expiration duration for an hour

---
+++ app/Http/Middleware/checkSessionTimeout.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpFoundation\Response;
+
+class checkSessionTimeout
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(! Auth::check() || !$request->session()->has('last_activity')) {
+            Auth::logout();
+            Alert::Error('Error', 'Unauthorized request for this resource, please login');
+            return redirect()->route('login');
+        }
+
+        $sessionLifeTime = config('session.lifetime') * 60;
+        $lastActivity = $request->session()->get('last_activity');
+
+        if(time() - $lastActivity > $sessionLifeTime) {
+            Auth::logout();
+            Alert::error('Error', 'Unauthorized request for this resource, please login');
+            return redirect()->route('login');
+        }
+
+        $request->session()->put('last_activity', time());
+        return $next($request);
+    }
+}
