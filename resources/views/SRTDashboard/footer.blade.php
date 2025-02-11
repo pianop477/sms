d3944d5a28a36998d8f97bee9b4fa54f0e851298
piano <pianop477@gmail.com>
2025-02-11 11:44:26 +0300

    piano company limited

--- resources/views/SRTDashboard/footer.blade.php
+++ resources/views/SRTDashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer-area fixed-bottom text-white">
         <span>&copy; Copyright - {{date('Y')}}</span> | Developed by <a href="" class="text-success fw-bold">
-            <strong>Piano</strong>
+            <strong>Piano Company Ltd</strong>
         </a>
     </div>
 </footer>
