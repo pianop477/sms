18b624474e28cdc8111be8ce04d3fdf52456f7aa
piano <pianop477@gmail.com>
2025-02-28 14:48:35 +0300

    shuleapp admin naogopa vijembe nimejitoa 😆😆

--- resources/views/SRTDashboard/footer.blade.php
+++ resources/views/SRTDashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer-area fixed-bottom text-white">
-        <span>&copy; Copyright - {{date('Y')}}</span> | Developed by <a href="" class="text-success fw-bold">
-            <strong>Piano</strong>
+        <span>&copy; Copyright - {{date('Y')}}</span>|<a href="" class="text-success fw-bold">
+            <strong>ShuleApp</strong>
         </a>
     </div>
 </footer>
