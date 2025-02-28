99d26133336057ee089b8bcf02a56f04d6dd4936
piano <pianop477@gmail.com>
2025-02-28 16:07:29 +0300

    commited for footer

--- resources/views/SRTDashboard/footer.blade.php
+++ resources/views/SRTDashboard/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-area fixed-bottom text-white">
-        <span>&copy; Copyright - {{date('Y')}}</span>|<a href="" class="text-success fw-bold">
+        <span>&copy; {{date('Y')}}</span> - <a href="" class="text-success fw-bold">
             <strong>ShuleApp</strong>
         </a>
     </div>
