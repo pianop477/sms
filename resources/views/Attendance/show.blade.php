b8a21394e42cec2d6bc2b7f2481b0f4478eb2c87
piano <pianop477@gmail.com>
2025-03-04 12:54:13 +0300

    jamani jamani Attendance ooohoooo!

--- resources/views/Attendance/show.blade.php
+++ resources/views/Attendance/show.blade.php
@@ -8,7 +8,7 @@
                     <h4 class="header-title">Attendance Report For: <span class="text-uppercase text-primary">{{ $firstRecord->student_firstname }} {{ $firstRecord->student_middlename }} {{ $firstRecord->student_lastname }}</span></h4>
                 </div>
                 <div class="col-2">
-                    <a href="{{route('attendance.byYear', 'student' => Hashids::encode($students->id))}}"><i class="fa-solid fa-circle-arrow-left text-secondary" style="font-size: 2rem"></i></a>
+                    <a href="{{route('attendance.byYear', ['student' => Hashids::encode($students->id)])}}"><i class="fa-solid fa-circle-arrow-left text-secondary" style="font-size: 2rem"></i></a>
                 </div>
             </div>
             <div class="single-table">
