5485ba2bd88f9bf2f4d6288d250dd0f478d052f8
piano <pianop477@gmail.com>
2025-03-12 17:08:56 +0300

    loader for blue background nimeihamisha

--- resources/views/SRTDashboard/topnavigation.blade.php
+++ resources/views/SRTDashboard/topnavigation.blade.php
@@ -1,4 +1,5 @@
 <div class="mainheader-area">
+    <div id="loading-bar" style="display:none; position: fixed; top: 0; left: 0; width: 100%; height: 4px; background: blue;"></div>
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-6">
