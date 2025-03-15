454c41d45966bb10c22a2b63993bfe9b576820b8
piano <pianop477@gmail.com>
2025-03-16 01:56:50 +0300

    amesema hivi sasa mfumo uko live na updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ForceToHttpsMiddleware::class,
+        \App\Http\Middleware\NoCacheHeaders::class, // Ongeza Middleware hii
     ];
 
     /**
