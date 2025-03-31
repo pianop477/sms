54e86b7ffc4cc9d3d0ff795b7355e48d127f039b
piano <pianop477@gmail.com>
2025-04-01 00:00:36 +0300

    no cache Middleware updated

--- app/Http/Middleware/NoCacheHeaders.php
+++ app/Http/Middleware/NoCacheHeaders.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class NoCacheHeaders
@@ -16,8 +17,19 @@ class NoCacheHeaders
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-                        ->header('Pragma', 'no-cache')
-                        ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+
+        if ($response instanceof BinaryFileResponse) {
+            // Tumia headers->set() kwa BinaryFileResponse
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        } else {
+            // Tumia header() kwa response ya kawaida
+            $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+                     ->header('Pragma', 'no-cache')
+                     ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        }
+
+        return $response;
     }
 }
