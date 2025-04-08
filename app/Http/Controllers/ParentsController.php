0d647da4b2925fc2ed68d33839ba0cace0076b50
piano <pianop477@gmail.com>
2025-04-08 15:07:02 +0300

    try and catch method added to import file

--- app/Http/Controllers/ParentsController.php
+++ app/Http/Controllers/ParentsController.php
@@ -13,6 +13,7 @@ use App\Models\Transport;
 use App\Models\User;
 use App\Services\BeemSmsService;
 use App\Services\NextSmsService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -516,99 +517,114 @@ class ParentsController extends Controller
     //import file
     public function import(Request $request)
     {
-        $request->validate([
-            'file' => 'required|file|mimes:xlsx,csv|max:2048',
-                ],
-                [
-                    'file.mimes' => 'The file must be a file of type: xlsx, csv.',
-                    'file.max' => 'The file may not be greater than 2MB.',
-                    'file.required' => 'The field must be filled.',
-                    'file.file' => 'The file must be a file.',
-        ]);
+        try {
+            $request->validate([
+                'file' => 'required|file|mimes:xlsx,csv|max:2048',
+                    ],
+                    [
+                        'file.mimes' => 'The file must be a file of type: xlsx, csv.',
+                        'file.max' => 'The file may not be greater than 2MB.',
+                        'file.required' => 'The field must be filled.',
+                        'file.file' => 'The file must be a file.',
+            ]);
+        } catch (Exception $e) {
+            Alert()->toast($e->getMessage(), 'error');
+            return back();
+        }
         /*
         PHP code for scanning files for viruses using virustotal.com API
         */
 
-        header("Content-Type: text/plain");
+       try {
+            header("Content-Type: text/plain");
 
-        // edit the virustotal.com api key, get one from the site
-        $virustotal_api_key = "7827171040f531d3cfe1ec05b05698819c3c1dcb7e476e1a26622a9fb646bbb1";
+            // edit the virustotal.com api key, get one from the site
+            $virustotal_api_key = "7827171040f531d3cfe1ec05b05698819c3c1dcb7e476e1a26622a9fb646bbb1";
 
-        // enter here the path of the file to be scanned
-        $file_to_scan = $request->file('file');
+            // enter here the path of the file to be scanned
+            $file_to_scan = $request->file('file');
 
-        // get the file size in mb, we will use it to know at what url to send for scanning (it's a different URL for over 30MB)
-        $file_size_mb = filesize($file_to_scan)/1024/1024;
+            // get the file size in mb, we will use it to know at what url to send for scanning (it's a different URL for over 30MB)
+            $file_size_mb = filesize($file_to_scan)/1024/1024;
 
-        // calculate a hash of this file, we will use it as an unique ID when quering about this file
-        $file_hash = hash('sha256', file_get_contents($file_to_scan));
-
-
-        // [PART 1] checking if a report for this file already exists (check by providing the file hash (md5/sha1/sha256)
-        // or by providing a scan_id that you receive when posting a new file for scanning
-        // !!! NOTE that scan_id is the only one that indicates file is queued/pending, the others will only report once scan is completed !!!
-        $report_url = 'https://www.virustotal.com/vtapi/v2/file/report?apikey='.$virustotal_api_key."&resource=".$file_hash;
-
-        $api_reply = file_get_contents($report_url);
-
-        // convert the json reply to an array of variables
-        $api_reply_array = json_decode($api_reply, true);
+            // calculate a hash of this file, we will use it as an unique ID when quering about this file
+            $file_hash = hash('sha256', file_get_contents($file_to_scan));
 
 
-        // your resource is queued for analysis
-        if($api_reply_array['response_code']==-2){
-            echo $api_reply_array['verbose_msg'];
-        }
+            // [PART 1] checking if a report for this file already exists (check by providing the file hash (md5/sha1/sha256)
+            // or by providing a scan_id that you receive when posting a new file for scanning
+            // !!! NOTE that scan_id is the only one that indicates file is queued/pending, the others will only report once scan is completed !!!
+            $report_url = 'https://www.virustotal.com/vtapi/v2/file/report?apikey='.$virustotal_api_key."&resource=".$file_hash;
 
-        // reply is OK (it contains an antivirus report)
-        // use the variables from $api_reply_array to process the antivirus data
-        if($api_reply_array['response_code']==1){
-            Alert()->toast('File already scanned, please check the report', 'info');
-            print_r($api_reply_array);
-            return back();
-            // exit;
-        }
+            $api_reply = file_get_contents($report_url);
 
-
-        // [PART 2] a report for this file was not found, upload file for scanning
-        if($api_reply_array['response_code']=='0'){
-
-            // default url where to post files
-            $post_url = 'https://www.virustotal.com/vtapi/v2/file/scan';
-
-            // get a special URL for uploading files larger than 32MB (up to 200MB)
-            if($file_size_mb >= 32){
-                $api_reply = @file_get_contents('https://www.virustotal.com/vtapi/v2/file/scan/upload_url?apikey='.$virustotal_api_key);
-                $api_reply_array = json_decode($api_reply, true);
-                if(isset($api_reply_array['upload_url']) and $api_reply_array['upload_url']!=''){
-                    $post_url = $api_reply_array['upload_url'];
-                }
-            }
-
-            // send a file for checking
-            // curl will accept an array here too:
-            $post['apikey'] = $virustotal_api_key;
-            $post['file'] = '@'.$file_to_scan;
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,$post_url);
-            curl_setopt($ch, CURLOPT_POST,1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-            $api_reply = curl_exec ($ch);
-            curl_close ($ch);
-
+            // convert the json reply to an array of variables
             $api_reply_array = json_decode($api_reply, true);
 
-            if($api_reply_array['response_code']==1){
-                echo "\nfile queued OK, you can use this scan_id to check the scan progress:\n".$api_reply_array['scan_id'];
-                echo "\nor just keep checking using the file hash, but it will only report once it is completed (no 'PENDING/QUEUED' reply will be given).";
-            }
-        }
 
-        Excel::import(new ParentStudentImport, $request->file('file'));
-        Alert()->toast('Records has been imported successfully', 'success');
-        return back();
+            // your resource is queued for analysis
+            if($api_reply_array['response_code']==-2){
+                echo $api_reply_array['verbose_msg'];
+            }
+
+            // reply is OK (it contains an antivirus report)
+            // use the variables from $api_reply_array to process the antivirus data
+            if($api_reply_array['response_code']==1){
+                Alert()->toast('File already scanned, please check the report', 'info');
+                print_r($api_reply_array);
+                return back();
+                // exit;
+            }
+
+
+            // [PART 2] a report for this file was not found, upload file for scanning
+            if($api_reply_array['response_code']=='0'){
+
+                // default url where to post files
+                $post_url = 'https://www.virustotal.com/vtapi/v2/file/scan';
+
+                // get a special URL for uploading files larger than 32MB (up to 200MB)
+                if($file_size_mb >= 32){
+                    $api_reply = @file_get_contents('https://www.virustotal.com/vtapi/v2/file/scan/upload_url?apikey='.$virustotal_api_key);
+                    $api_reply_array = json_decode($api_reply, true);
+                    if(isset($api_reply_array['upload_url']) and $api_reply_array['upload_url']!=''){
+                        $post_url = $api_reply_array['upload_url'];
+                    }
+                }
+
+                // send a file for checking
+                // curl will accept an array here too:
+                $post['apikey'] = $virustotal_api_key;
+                $post['file'] = '@'.$file_to_scan;
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$post_url);
+                curl_setopt($ch, CURLOPT_POST,1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                $api_reply = curl_exec ($ch);
+                curl_close ($ch);
+
+                $api_reply_array = json_decode($api_reply, true);
+
+                if($api_reply_array['response_code']==1){
+                    echo "\nfile queued OK, you can use this scan_id to check the scan progress:\n".$api_reply_array['scan_id'];
+                    echo "\nor just keep checking using the file hash, but it will only report once it is completed (no 'PENDING/QUEUED' reply will be given).";
+                }
+            }
+       } catch(Exception $e) {
+            Alert()->toast($e->getMessage(), 'error');
+            return back();
+        }
+        try {
+
+            Excel::import(new ParentStudentImport, $request->file('file'));
+            Alert()->toast('Records has been imported successfully', 'success');
+            return back();
+        } catch (Exception $e) {
+            Alert()->toast($e->getMessage(), 'error');
+            return back();
+        }
     }
 
     public function exportFile()
