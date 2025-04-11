ae69e90c14bc964f84c33d2d6f88628ec1a9d096
piano <pianop477@gmail.com>
2025-04-11 12:27:25 +0300

    eeeh epiphan ukubali basi acha kuloga

--- resources/views/Examinations/prepare_form.blade.php
+++ resources/views/Examinations/prepare_form.blade.php
@@ -110,7 +110,6 @@
                             </div>
                            </div>
                         </div>
-                        <p>{{$saved_results->first()->exam_term}}</p>
                         <div class="col-6">
                             <button class="btn btn-primary float-right" id="saveButton" type="submit">
                                 Next Step <i class="ti-arrow-right"></i>
