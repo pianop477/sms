b0cf87d8f8055755ebe09477bb6ad595361748e6
piano <pianop477@gmail.com>
2025-04-24 13:02:55 +0300

    invalid feedback removed

--- resources/views/profile/change-password.blade.php
+++ resources/views/profile/change-password.blade.php
@@ -16,7 +16,7 @@
                     <div class="col-md-4 mb-3">
                         <label for="currentPassword">Current Password</label>
                         <div class="input-group">
-                            <input type="password" name="current_password" class="form-control @error('current_password') invalid-feedback @enderror" id="currentPassword" placeholder="" value="{{old('current_password')}}" required>
+                            <input type="password" name="current_password" class="form-control" id="currentPassword" placeholder="" value="{{old('current_password')}}" required>
                             <div class="input-group-append">
                                 <span class="input-group-text toggle-password" data-target="currentPassword">
                                     <i class="ti-eye"></i>
@@ -33,7 +33,7 @@
                     <div class="col-md-4 mb-3">
                         <label for="newPassword">New Password</label>
                         <div class="input-group">
-                            <input type="password" name="new_password" class="form-control @error('new_password') invalid-feedback @enderror" id="newPassword" placeholder="" required value="{{old('new_password')}}">
+                            <input type="password" name="new_password" class="form-control" id="newPassword" placeholder="" required value="{{old('new_password')}}">
                             <div class="input-group-append">
                                 <span class="input-group-text toggle-password" data-target="newPassword">
                                     <i class="ti-eye"></i>
@@ -51,7 +51,7 @@
                     <div class="col-md-4 mb-3">
                         <label for="confirmPassword">Confirm Password</label>
                         <div class="input-group">
-                            <input type="password" name="confirm_password" class="form-control @error('confirm_password') invalid-feedback @enderror" id="confirmPassword" placeholder="" required value="{{old('confirm_password')}}">
+                            <input type="password" name="confirm_password" class="form-control" id="confirmPassword" placeholder="" required value="{{old('confirm_password')}}">
                             <div class="input-group-append">
                                 <span class="input-group-text toggle-password" data-target="confirmPassword">
                                     <i class="ti-eye"></i>
