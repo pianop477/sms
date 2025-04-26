da7aa270a39e90e80ba471fbdd0e97f335a4f974
piano <pianop477@gmail.com>
2025-04-26 19:11:41 +0300

    favicon changed

--- resources/views/SRTDashboard/header.blade.php
+++ resources/views/SRTDashboard/header.blade.php
@@ -3,7 +3,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Shule | App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+    <link rel="shortcut icon" type="image/png" href="{{asset('assets/img/favicon/favicon.ico')}}">
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/fontawesome-free-6.5.2-web/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/slicknav.min.css')}}">
-    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon/favicon.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon/favicon-16x16.png')}}">
     <!-- amchart css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
     <!-- others css -->
