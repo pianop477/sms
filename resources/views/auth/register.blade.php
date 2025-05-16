806cef58cb3999650d5b8b4ff06cedbc4836aecb
piano <pianop477@gmail.com>
2025-05-16 15:53:11 +0300

    registration page for parents locked

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,60 +15,61 @@
                         <h4>Parents Registration Form</h4>
                     </div>
                     <div class="login-form-body">
-                        <div class="form-gp">
+                        <h4 class="text-danger text-center">Please Contact Admin for registration or support</h4>
+                        {{-- <div class="form-gp">
                             <label for="exampleInputName1">First Name</label>
                             <input type="text" id="exampleInputName1" name="fname" value="{{old('fname')}}">
                             <i class="ti-user"></i>
                             @error('fname')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
-                        <div class="form-gp">
+                        </div> --}}
+                        {{-- <div class="form-gp">
                             <label for="exampleInputName2">Last Name</label>
                             <input type="text" id="exampleInputName2" name="lname" value="{{old('lname')}}">
                             <i class="ti-user"></i>
                             @error('lname')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
-                        <div class="form-gp">
+                        </div> --}}
+                        {{-- <div class="form-gp">
                             <label for="exampleInputEmail1">Email</label>
                             <input type="email" id="exampleInputEmail1" name="email" value="{{old('email')}}">
                             <i class="ti-email"></i>
                             @error('email')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
-                        <div class="form-gp">
+                        </div> --}}
+                        {{-- <div class="form-gp"> --}}
                             {{-- <label for="exampleInputGender">Gender</label> --}}
-                            <select name="gender" id="exampleInputGender" class="form-control">
+                            {{-- <select name="gender" id="exampleInputGender" class="form-control">
                                 <option value="">--Select Gender--</option>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                             </select>
                             @error('gender')
                                 <div class="text-danger">{{$message}}</div>
-                            @enderror
-                        </div>
-                        <div class="form-gp">
+                            @enderror --}}
+                        {{-- </div> --}}
+                        {{-- <div class="form-gp">
                             <label for="exampleInputPhone">Phone Number</label>
                             <input type="tel" id="exampleInputPhone" name="phone" value="{{old('phone')}}">
                             <i class="ti-mobile"></i>
                             @error('phone')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
-                        <div class="form-gp">
+                        </div> --}}
+                        {{-- <div class="form-gp">
                             <label for="exampleInputStreet">Street Name/Location</label>
                             <input type="text" id="exampleInputStreet" name="street" value="{{old('street')}}">
                             <i class="ti-location-pin"></i>
                             @error('street')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
+                        </div> --}}
 
                         <!-- Password -->
-                        <div class="form-gp">
+                        {{-- <div class="form-gp">
                             <label for="exampleInputPassword1">Password</label>
                             <div class="input-group">
                                 <input type="password" id="exampleInputPassword1" name="password" placeholder="Password" class="form-control">
@@ -81,10 +82,10 @@
                             @error('password')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
+                        </div> --}}
 
                         <!-- Confirm Password -->
-                        <div class="form-gp">
+                        {{-- <div class="form-gp">
                             <label for="exampleInputPassword2">Repeat Password</label>
                             <div class="input-group">
                                 <input type="password" id="exampleInputPassword2" name="password_confirmation" placeholder="Repeat Password" class="form-control">
@@ -97,11 +98,11 @@
                             @error('password_confirmation')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
-                        </div>
+                        </div> --}}
 
-                        <div class="form-gp">
+                        {{-- <div class="form-gp"> --}}
                             {{-- <label for="exampleInputSchool">School Name</label> --}}
-                            <select name="school" id="exampleInputSchool" class="form-control text-capitalize">
+                            {{-- <select name="school" id="exampleInputSchool" class="form-control text-capitalize">
                                 <option value="">--Select School Name--</option>
                                 @if ($schools->isEmpty())
                                     <option value="" class="text-danger">No schools found</option>
@@ -113,14 +114,14 @@
                             </select>
                             @error('school')
                                 <div class="text-danger">{{$message}}</div>
-                            @enderror
-                        </div>
+                            @enderror --}}
+                        {{-- </div> --}}
 
-                        <div class="submit-btn-area">
+                        {{-- <div class="submit-btn-area">
                             <button id="saveButton" type="submit">Submit <i class="ti-arrow-right"></i></button>
-                        </div>
+                        </div> --}}
                         <div class="form-footer text-center mt-4">
-                            <p class="text-muted">Do you have an Account? <a href="{{route('login')}}">Login here</a></p>
+                            <p class="text-muted">Go Back <a href="{{route('welcome')}}" class="btn btn-success">Home</a></p>
                         </div>
                     </div>
                 </form>
