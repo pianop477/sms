14c4cbbb5a5e30d273977b4d2d42c22ddf6da9ba
piano <pianop477@gmail.com>
2025-07-16 17:41:07 +0300

    php umetisha aisee ku export hizi files

--- resources/views/Results/class_pdf_results.blade.php
+++ resources/views/Results/class_pdf_results.blade.php
@@ -4,12 +4,17 @@
 <div class="card mt-2">
     <div class="card-body">
         <div class="row">
-            <div class="col-md-10">
+            <div class="col-md-8">
                 <h4 class="header-title text-uppercase">{{$results->first()->class_name}} - results for {{$results->first()->exam_type}} ({{$date}})</h4>
             </div>
             <div class="col-md-2">
                 <a href="{{route('results.monthsByExamType', ['school' => Hashids::encode($schools->id), 'year' => $year, 'class' => Hashids::encode($class_id), 'examType' => Hashids::encode($exam_id), 'month' => $month])}}" class="btn btn-info btn-xs float-right">
                     <i class="fas fa-arrow-circle-left"></i> Back
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="{{ url()->current() }}?export_excel=1" class="btn btn-success btn-xs float-right">
+                    <i class="fas fa-file-excel"></i> Export Excel
                 </a>
             </div>
         </div>
