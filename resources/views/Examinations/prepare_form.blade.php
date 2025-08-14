21e25638aa7becfdc8aedf200bf30d2b05ceb217
piano <pianop477@gmail.com>
2025-08-14 09:23:23 +0300

    check it out

--- resources/views/Examinations/prepare_form.blade.php
+++ resources/views/Examinations/prepare_form.blade.php
@@ -74,12 +74,12 @@
                     </div>
                 </div>
                 @if ($saved_results->isEmpty())
-                    <button class="btn btn-primary float-left" id="saveButton" type="submit">
+                    <button class="btn btn-primary float-right" id="saveButton" type="submit">
                         Save & Proceed <i class="ti-arrow-right"></i>
                     </button>
                 @else
-                    <div class="col-12 justify-content-between d-flex">
-                        <button class="btn btn-primary float-left" id="saveButton" type="submit">
+                    <div class="col-12">
+                        <button class="btn btn-primary float-right" id="saveButton" type="submit">
                             Save & Proceed <i class="ti-arrow-right"></i>
                         </button>
                     </div>
