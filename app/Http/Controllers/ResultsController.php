22b80dfed6df648d6f8cf6905076dbac6cddcaeb
piano <pianop477@gmail.com>
2026-01-05 19:44:24 +0300

    try to check if student exists

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -3440,8 +3440,16 @@ class ResultsController extends Controller
             $finalData[] = compact('subjectName', 'teacher', 'subjectCode', 'examScores', 'total', 'average', 'position');
         }
 
-        $students = $results->first();
+        // $students = $results->first();
         $schoolInfo = $results->first();
+
+        $studentModel = Student::find($studentId);
+
+        if (!$studentModel) {
+            // return back()->with('error', 'Mwanafunzi hakupatikana.');
+            Alert()->toast('Student not found.', 'error');
+            return redirect()->back();
+        }
         // return $schoolInfo;
 
         // =================== EXAM HEADERS WITH DATES ===================
