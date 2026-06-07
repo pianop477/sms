56cc31d80409e1ed58ff47441a6ceedc2e635f95
piano <pianop477@gmail.com>
2026-06-07 11:43:49 +0300

    fix: do not force parent password change

--- app/Http/Middleware/ForcePasswordChange.php
+++ app/Http/Middleware/ForcePasswordChange.php
@@ -23,14 +23,26 @@ class ForcePasswordChange
             return $next($request);
         }
 
-        // Acha aendelee kama yupo kwenye page ya kubadilisha password
+        $user = Auth::user();
+        $userType = $user->usertype; // assuming column name is 'usertype'
+
+        // Acha aendelee kama yupo kwenye page ya kubadilisha password au logout
         if ($request->routeIs('change.password') || $request->routeIs('change.new.password') || $request->routeIs('logout')) {
             return $next($request);
         }
 
         // Check kama anatumia default password (mfano: shule2025)
-        if (Hash::check('shule2025', Auth::user()->password)) {
-            // Alert::warning('Warning', 'Default password is no longer allowed, Please change it now!');
+        $isUsingDefaultPassword = Hash::check('shule2025', $user->password);
+
+        // Kama ni parent (usertype 4), acha aendelee hata kama anatumia default password
+        if ($userType == 4) {
+            return $next($request);
+        }
+
+        // Kwa usertype wengine (1,2,3), watalazimika kubadilisha password
+        if ($isUsingDefaultPassword && in_array($userType, [1, 2, 3])) {
+            // Unaweza kutumia session au alert kuwataarifu
+            session()->flash('warning', 'Default password is no longer allowed. Please change your password now!');
             return to_route('change.password');
         }
 
