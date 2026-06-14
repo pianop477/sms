e60c45495de4b7fd08f30f93c46b767d7b865c98
piano <pianop477@gmail.com>
2026-06-14 23:37:27 +0300

    try: change class name to avoid name repetition

--- database/migrations/2026_06_14_224034_add_extra_columns_to_fee_clearance_tokens_table.php
+++ database/migrations/2026_06_14_224034_add_extra_columns_to_fee_clearance_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNotificationSentAndUniqueToFeeClearanceTokens extends Migration
+class AddNotificationSentColumnToFeeClearanceTokens extends Migration
 {
     public function up()
     {
