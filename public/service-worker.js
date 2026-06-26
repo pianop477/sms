67ac966b0ef79c57dd1f1d4c1091329cc550b4dd
pianop477 <pianop477@gmail.com>
2026-06-26 17:08:33 -0400

    try to add new worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.06.20.02';
+const APP_VERSION = '2026.06.27.00';
 const CACHE_NAME = `shuleapp-cache-${APP_VERSION}`;
 const TOKEN_DB_NAME = 'gatepass-tokens-db';
 const TOKEN_STORE_NAME = 'tokens';
